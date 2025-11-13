c8875c04a46ac62ff92abacde8a648d5e54bb9eb
Larissa Ferreira <larissadferr@gmail.com>
2025-11-13 16:12:58 -0300

    added fish to database, fishList

---
+++ scripts/aquariumData.js
@@ -0,0 +1,60 @@
+export const database = {
+    fish: [
+        {
+            name: "Nemo",
+            species: "Clownfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Amphiprion_ocellaris_%28Clown_anemonefish%29_by_Nick_Hobgood.jpg"
+        },
+        {
+            name: "Dory",
+            species: "Blue Tang",
+            length: 5,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/13/Paletten-Doktorfisch_M%C3%BCnster.JPG"
+        },
+        {
+            name: "Gill",
+            species: "Moorish Idol",
+            length: 8,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/7/71/%C3%8Ddolo_moro_%28Zanclus_cornutus%29%2C_Anilao%2C_Filipinas%2C_2023-08-25%2C_DD_233.jpg"
+        },
+        {
+            name: "Jacques",
+            species: "Pacific Cleaner Shrimp",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Lysmata_amboinensis_in_Tropicarium-Oceanarium_Budapest.JPG/800px-Lysmata_amboinensis_in_Tropicarium-Oceanarium_Budapest.JPG"
+        },
+        {
+            name: "Gurgle",
+            species: "Royal Gramma",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Plantivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Gramma_loreto%2C_Adult_%28Royal_Gramma%29.jpg/800px-Gramma_loreto%2C_Adult_%28Royal_Gramma%29.jpg"
+        },
+        {
+            name: "Deb",
+            species: "Humbug Damselfish",
+            length: 3,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Humbug_dascyllus_%28Dascyllus_aruanus%29_%2848652156722%29.jpg"
+        },
+        {
+            name: "Bloat",
+            species: "Porcupine Pufferfish",
+            length: 20,
+            location: "Great Barrier Reef",
+            diet: "Omnivore",
+            image: "https://upload.wikimedia.org/wikipedia/commons/1/18/Pez_erizo_de_espinas_largas_%28Diodon_holocanthus%29%2C_islas_Ad_Dimaniyat%2C_Om%C3%A1n%2C_2024-08-13%2C_DD_45.jpg"
+        }
+    ]
+}
