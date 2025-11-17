1a08a2dfaa2159d73fc97f8af7b078e55fcfaeb8
Larissa Ferreira <larissadferr@gmail.com>
2025-11-17 16:58:00 -0300

    added html string generators to holyFish, soldierFish, and regularFish

---
+++ scripts/holyFish.js
@@ -0,0 +1,23 @@
+import { database } from './aquariumData.js';
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const holyFish = ""
+
+    for (const fish of database.fish) {
+        if (fish.length % 3 === 0) {
+            holyFish += `
+            <section class="fish fish--holy">
+                <img src="${fish.image}" alt="${fish.name} image" class="fish-image"></img>
+                <h3 class="fish-name">${fish.name}</h3>
+                <p class="fish-species">Species: ${fish.species}</p>
+                <p class="fish-length">Length: ${fish.length}</p>
+                <p class="fish-location">Location: ${fish.location}</p>
+                <p class="fish-diet">Diet: ${fish.diet}</p>
+            </section>`
+        }
+
+    }
+
+    return holyFish
+}
---
+++ scripts/regularFish.js
@@ -0,0 +1,23 @@
+import { database } from './aquariumData.js';
+
+export const regularFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const regularFish = ""
+
+    for (const fish of database.fish) {
+        if (!(fish.length % 5 === 0) && !(fish.length % 3 === 0)) {
+            regularFish += `
+            <section class="fish fish--holy">
+                <img src="${fish.image}" alt="${fish.name} image" class="fish-image"></img>
+                <h3 class="fish-name">${fish.name}</h3>
+                <p class="fish-species">Species: ${fish.species}</p>
+                <p class="fish-length">Length: ${fish.length}</p>
+                <p class="fish-location">Location: ${fish.location}</p>
+                <p class="fish-diet">Diet: ${fish.diet}</p>
+            </section>`
+        }
+
+    }
+
+    return regularFish
+}
